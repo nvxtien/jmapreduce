0e177f734f13808da060c68bae92cb1eecdec52e
tien.nguyenv <tien.nguyenv@smartdev.vn>
2016-02-02 17:50:13 +0700

    update test

--- test/jmapreduce.js
+++ test/jmapreduce.js
@@ -8,7 +8,7 @@ var expect = require('chai').expect;
 var JMapReduce = require('../lib/jmapreduce.js');
 
 describe('should return lines', function () {
-    it('split line', function(done){
+    it('split line', function (done) {
         function lines(data) {
             return data.match(/[^\r\n]+/g);
         };
@@ -21,13 +21,13 @@ describe('should return lines', function () {
 });
 
 describe('should return key value', function () {
-    it('key value', function(done){
+    it('key value', function (done) {
         var jmapReduce = new JMapReduce();
         jmapReduce.textData("line1 line1\n\n line2\n")
             .flatMap(function (data) {
                 return data.match(/[^\s]+|\s+[^\s+]$/g);
             })
-            .map(function(x){
+            .map(function (x) {
                 return {key: x, value: 1};
             })
         ;
@@ -42,42 +42,49 @@ describe('should return key value', function () {
 });
 
 describe('should return words key value', function () {
-    var jmapReduce = new JMapReduce();
-    jmapReduce.textData("group1 group3\n\n group3\n group3\n\n group3 group1 group2\n")
+    it('words key value', function (done) {
+        var jmapReduce = new JMapReduce();
+        jmapReduce.textData("group1 group3\n\n group3\n group3\n\n group3 group1 group2\n")
             .flatMap(function (data) {
                 return data.match(/[^\s]+|\s+[^\s+]$/g);
             })
-            .map(function(x){
+            .map(function (x) {
                 return {key: x, value: 1};
             })
             .groupByKey();
 
-    //jmapReduce.print();
-    var collect = jmapReduce.collect();
-    expect(diff(jmapReduce.collect()[0], {key: 'group1', value: [{key: 'group1', value: 1}, {key: 'group1', value: 1}]}))
-        .to.equal(false);
+        //jmapReduce.print();
+        var collect = jmapReduce.collect();
+        expect(diff(jmapReduce.collect()[0], {
+            key: 'group1',
+            value: [{key: 'group1', value: 1}, {key: 'group1', value: 1}]
+        }))
+            .to.equal(false);
 
-    expect(diff(collect[2], {key: 'group2', value: [{key: 'group2', value: 1}]})).to.equal(false);
-
+        expect(diff(collect[2], {key: 'group2', value: [{key: 'group2', value: 1}]})).to.equal(false);
+        done();
+    });
 });
 
 describe('should print', function () {
-    var jmapReduce = new JMapReduce();
-    jmapReduce.textData("line1 \n\n line2\n line3\n\n  line4\n")
+    it('print', function (done) {
+        var jmapReduce = new JMapReduce();
+        jmapReduce.textData("line1 \n\n line2\n line3\n\n  line4\n")
             .flatMap(function (data) {
                 return data.match(/[^\s]+|\s+[^\s+]$/g);
-            }).map(function(x){
-                return {key: x, value: 1};
-    }       );
-
-    //jmapReduce.print();
-    console.log('======================================================');
+            }).map(function (x) {
+            return {key: x, value: 1};
+        });
+        //jmapReduce.print();
+        done();
+    });
 });
 
 describe('should return sorted array', function () {
-    var jmapReduce = new JMapReduce();
-    var input = "group1 group3\n\n group3\n group3\n\n group3 group1 group2\n";
-    jmapReduce.textData(input)
+    it('sorted array', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = "group1 group3\n\n group3\n group3\n\n group3 group1 group2\n";
+        jmapReduce.textData(input)
             .flatMap(function (data) {
                 return data.match(/[^\s]+|\s+[^\s+]$/g);
             })
@@ -92,112 +99,128 @@ describe('should return sorted array', function () {
                 return b.value - a.value;
             });
 
-    expect(diff(jmapReduce.collect()[0], {key: 'group3', value: 4})).to.equal(false);
-    expect(diff(jmapReduce.collect()[1], {key: 'group1', value: 2})).to.equal(false);
-    expect(diff(jmapReduce.collect()[2], {key: 'group2', value: 1})).to.equal(false);
-
+        expect(diff(jmapReduce.collect()[0], {key: 'group3', value: 4})).to.equal(false);
+        expect(diff(jmapReduce.collect()[1], {key: 'group1', value: 2})).to.equal(false);
+        expect(diff(jmapReduce.collect()[2], {key: 'group2', value: 1})).to.equal(false);
+        done();
+    });
 });
 
 describe('with input is an array', function () {
-    var jmapReduce = new JMapReduce();
-    var input = ["group1 group3 \n\ngroup3\n group3\n\n group3 group1 group2 \n", 'group5 group6'];
-    jmapReduce.textData(input);
+    it('input array', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = ["group1 group3 \n\ngroup3\n group3\n\n group3 group1 group2 \n", 'group5 group6'];
+        jmapReduce.textData(input);
 
-    //jmapReduce.print();
+        //jmapReduce.print();
 
-    expect(jmapReduce.collect()[0]).to.equal("group1 group3 \n\ngroup3\n group3\n\n group3 group1 group2 \n");
-    expect(jmapReduce.collect()[1]).to.equal("group5 group6");
+        expect(jmapReduce.collect()[0]).to.equal("group1 group3 \n\ngroup3\n group3\n\n group3 group1 group2 \n");
+        expect(jmapReduce.collect()[1]).to.equal("group5 group6");
 
-    jmapReduce.flatMap(function(x){
-        return x.match(/[^\s]+|\s+[^\s+]$/g);
-    });
-
-    jmapReduce.print();
-
-    expect(jmapReduce.collect()[0]).to.equal('group1');
-    expect(jmapReduce.collect()[1]).to.equal('group3');
-
-    /*expect(diff(jmapReduce.collect()[0], {key: 'group1', value: 1})).to.equal(false);
-    expect(diff(jmapReduce.collect()[1], {key: 'group3', value: 1})).to.equal(false);*/
-
-    jmapReduce.map(function(x){
-        return {key: x, value: 1};
-    });
-
-    expect(diff(jmapReduce.collect()[0], {key: 'group1', value: 1})).to.equal(false);
-    expect(diff(jmapReduce.collect()[1], {key: 'group3', value: 1})).to.equal(false);
-
-});
-
-describe('with input is only string', function () {
-    var jmapReduce = new JMapReduce();
-    var input = "input is only string";
-    jmapReduce.textData(input)
-        .flatMap(function(x){
+        jmapReduce.flatMap(function (x) {
             return x.match(/[^\s]+|\s+[^\s+]$/g);
-        })
-        .map(function(x){
+        });
+
+        jmapReduce.print();
+
+        expect(jmapReduce.collect()[0]).to.equal('group1');
+        expect(jmapReduce.collect()[1]).to.equal('group3');
+
+        /*expect(diff(jmapReduce.collect()[0], {key: 'group1', value: 1})).to.equal(false);
+         expect(diff(jmapReduce.collect()[1], {key: 'group3', value: 1})).to.equal(false);*/
+
+        jmapReduce.map(function (x) {
             return {key: x, value: 1};
         });
 
-    expect(diff(jmapReduce.collect()[0], {key: 'input', value: 1})).to.equal(false);
-    expect(diff(jmapReduce.collect()[1], {key: 'is', value: 1})).to.equal(false);
-    expect(diff(jmapReduce.collect()[2], {key: 'only', value: 1})).to.equal(false);
-    expect(diff(jmapReduce.collect()[3], {key: 'string', value: 1})).to.equal(false);
+        expect(diff(jmapReduce.collect()[0], {key: 'group1', value: 1})).to.equal(false);
+        expect(diff(jmapReduce.collect()[1], {key: 'group3', value: 1})).to.equal(false);
 
-});
-
-describe('textData with string', function(){
-    var jmapReduce = new JMapReduce();
-    var input = 'read text data';
-
-    jmapReduce.textData(input);
-    expect(jmapReduce.collect()[0]).to.equal("read text data");
-});
-
-describe('textData with array of strings', function(){
-    var jmapReduce = new JMapReduce();
-    var input = ['textData reads', 'array', 'of strings'];
-
-    jmapReduce.textData(input);
-    expect(jmapReduce.collect()[0]).to.equal('textData reads');
-    expect(jmapReduce.collect()[1]).to.equal('array');
-    expect(jmapReduce.collect()[2]).to.equal('of strings');
-});
-
-describe('flatMap', function(){
-    var jmapReduce = new JMapReduce();
-    var input = ['textData reads', 'array', 'of strings'];
-
-    jmapReduce.textData(input);
-    expect(jmapReduce.collect()[0]).to.equal('textData reads');
-    expect(jmapReduce.collect()[1]).to.equal('array');
-    expect(jmapReduce.collect()[2]).to.equal('of strings');
-
-    jmapReduce.flatMap(function(x){
-        return x.match(/[^\s]+|\s+[^\s+]$/g);
+        done();
     });
+});
 
-    expect(jmapReduce.collect()[0]).to.equal('textData');
-    expect(jmapReduce.collect()[1]).to.equal('reads');
-    expect(jmapReduce.collect()[2]).to.equal('array');
-    expect(jmapReduce.collect()[3]).to.equal('of');
-    expect(jmapReduce.collect()[4]).to.equal('strings');
+describe('with input is only string', function () {
+    it('only string', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = "input is only string";
+        jmapReduce.textData(input)
+            .flatMap(function (x) {
+                return x.match(/[^\s]+|\s+[^\s+]$/g);
+            })
+            .map(function (x) {
+                return {key: x, value: 1};
+            });
+
+        expect(diff(jmapReduce.collect()[0], {key: 'input', value: 1})).to.equal(false);
+        expect(diff(jmapReduce.collect()[1], {key: 'is', value: 1})).to.equal(false);
+        expect(diff(jmapReduce.collect()[2], {key: 'only', value: 1})).to.equal(false);
+        expect(diff(jmapReduce.collect()[3], {key: 'string', value: 1})).to.equal(false);
+
+        done();
+    });
+});
+
+describe('textData with string', function () {
+    it('with string', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = 'read text data';
+
+        jmapReduce.textData(input);
+        expect(jmapReduce.collect()[0]).to.equal("read text data");
+        done();
+    });
+});
+
+describe('textData with array of strings', function () {
+    it('array of string', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = ['textData reads', 'array', 'of strings'];
+
+        jmapReduce.textData(input);
+        expect(jmapReduce.collect()[0]).to.equal('textData reads');
+        expect(jmapReduce.collect()[1]).to.equal('array');
+        expect(jmapReduce.collect()[2]).to.equal('of strings');
+        done();
+    });
+});
+
+describe('flatMap', function () {
+    it('flatMap', function (done) {
+        var jmapReduce = new JMapReduce();
+        var input = ['textData reads', 'array', 'of strings'];
+
+        jmapReduce.textData(input);
+        expect(jmapReduce.collect()[0]).to.equal('textData reads');
+        expect(jmapReduce.collect()[1]).to.equal('array');
+        expect(jmapReduce.collect()[2]).to.equal('of strings');
+
+        jmapReduce.flatMap(function (x) {
+            return x.match(/[^\s]+|\s+[^\s+]$/g);
+        });
+
+        expect(jmapReduce.collect()[0]).to.equal('textData');
+        expect(jmapReduce.collect()[1]).to.equal('reads');
+        expect(jmapReduce.collect()[2]).to.equal('array');
+        expect(jmapReduce.collect()[3]).to.equal('of');
+        expect(jmapReduce.collect()[4]).to.equal('strings');
+        done();
+    })
 });
 
 /*describe('textData with array of array', function(){
-    var jmapReduce = new JMapReduce();
-    var input = [['textData reads', 'array', 'of strings'], ['test', 'data'], ['data', 'for test']];
+ var jmapReduce = new JMapReduce();
+ var input = [['textData reads', 'array', 'of strings'], ['test', 'data'], ['data', 'for test']];
 
-    jmapReduce.textData(input);
+ jmapReduce.textData(input);
 
-    jmapReduce.flatMap(function(x){
-        return x.match(/[^\s]+|\s+[^\s+]$/g);
-    });
+ jmapReduce.flatMap(function(x){
+ return x.match(/[^\s]+|\s+[^\s+]$/g);
+ });
 
-    //jmapReduce.print();
+ //jmapReduce.print();
 
-    expect(jmapReduce.collect()[0][0]).to.equal('textData reads');
-    expect(jmapReduce.collect()[1][0]).to.equal('test');
-    expect(jmapReduce.collect()[2][0]).to.equal('data');
-});*/
+ expect(jmapReduce.collect()[0][0]).to.equal('textData reads');
+ expect(jmapReduce.collect()[1][0]).to.equal('test');
+ expect(jmapReduce.collect()[2][0]).to.equal('data');
+ });*/
